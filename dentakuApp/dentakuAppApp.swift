45e06ffab453f16e7106e6894356955decabe5eb
carrot-99 <kaito.01204@gmail.com>
2023-02-25 13:09:51 +0900

    first commit

---
+++ dentakuApp/dentakuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dentakuAppApp.swift
+//  dentakuApp
+//
+//  Created by USER on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct dentakuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
